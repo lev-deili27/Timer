c3097a22c243e6cc452710ecc58bc48f47073aa6
lev-deili27 <45765020+lev-deili27@users.noreply.github.com>
2019-04-09 22:17:41 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Mistral", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(88, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00:00";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(75, 280);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TabStop = false;
+            this.textBox1.Tag = "";
+            this.textBox1.Text = "Enter time in seconds";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Enabled = false;
+            this.button3.Image = global::Timer.Properties.Resources.Image3;
+            this.button3.Location = new System.Drawing.Point(151, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 25);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Enabled = false;
+            this.button2.Image = global::Timer.Properties.Resources.Image2;
+            this.button2.Location = new System.Drawing.Point(117, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 25);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Enabled = false;
+            this.button1.Image = global::Timer.Properties.Resources.Image1;
+            this.button1.Location = new System.Drawing.Point(83, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 25);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pictureBox1.Image = global::Timer.Properties.Resources.timer_60;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 246);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.Location = new System.Drawing.Point(198, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(36, 20);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "ok";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(270, 312);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        int m = 0;
+        int s = 0;
+        int k = 0;
+        string time;
+        bool flag_pause = false;
+        bool flag_stop = false;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "00:00";
+            pictureBox1.Image = Timer.Properties.Resources.timer_60;
+            flag_stop = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (s == 0)
+            {
+                if (m > 0)
+                {
+                    m--;
+                    s = 60;
+                }
+                else
+                {
+                    timer1.Enabled = false;
+                    s = 0;
+                    k = 0;
+                    label1.Text = "00:00";
+                    pictureBox1.Image = Timer.Properties.Resources.timer_60;
+                }
+              
+            }
+            if (s > 0)
+            {
+                s--;
+            }
+            if (m < 10 && s < 10)
+            {
+                label1.Text = "0"+m.ToString() + ":" + "0" + s.ToString();
+            }
+            else if (s < 10)
+            {
+                label1.Text = m.ToString() + ":" + "0"+s.ToString();
+            }
+            else if (m < 10)
+            {
+                label1.Text = "0"+m.ToString() + ":" + s.ToString();
+            }
+            else
+            {
+                label1.Text = m.ToString() + ":" + s.ToString();
+            }
+            k = s % 60;
+            k--;
+            if (k == 1) { pictureBox1.Image = Timer.Properties.Resources.timer_0; }
+            if (k == 2) { pictureBox1.Image = Timer.Properties.Resources.timer_1; }
+            if (k == 3) { pictureBox1.Image = Timer.Properties.Resources.timer_2; }
+            if (k == 4) { pictureBox1.Image = Timer.Properties.Resources.timer_3; }
+            if (k == 5) { pictureBox1.Image = Timer.Properties.Resources.timer_4; }
+            if (k == 6) { pictureBox1.Image = Timer.Properties.Resources.timer_5; }
+            if (k == 7) { pictureBox1.Image = Timer.Properties.Resources.timer_6; }
+            if (k == 8) { pictureBox1.Image = Timer.Properties.Resources.timer_7; }
+            if (k == 9) { pictureBox1.Image = Timer.Properties.Resources.timer_8; }
+            if (k == 10) { pictureBox1.Image = Timer.Properties.Resources.timer_9; }
+            if (k == 11) { pictureBox1.Image = Timer.Properties.Resources.timer_10; }
+            if (k == 12) { pictureBox1.Image = Timer.Properties.Resources.timer_11; }
+            if (k == 13) { pictureBox1.Image = Timer.Properties.Resources.timer_12; }
+            if (k == 14) { pictureBox1.Image = Timer.Properties.Resources.timer_13; }
+            if (k == 15) { pictureBox1.Image = Timer.Properties.Resources.timer_14; }
+            if (k == 16) { pictureBox1.Image = Timer.Properties.Resources.timer_15; }
+            if (k == 17) { pictureBox1.Image = Timer.Properties.Resources.timer_16; }
+            if (k == 18) { pictureBox1.Image = Timer.Properties.Resources.timer_17; }
+            if (k == 19) { pictureBox1.Image = Timer.Properties.Resources.timer_18; }
+            if (k == 20) { pictureBox1.Image = Timer.Properties.Resources.timer_19; }
+            if (k == 21) { pictureBox1.Image = Timer.Properties.Resources.timer_20; }
+            if (k == 22) { pictureBox1.Image = Timer.Properties.Resources.timer_21; }
+            if (k == 23) { pictureBox1.Image = Timer.Properties.Resources.timer_22; }
+            if (k == 24) { pictureBox1.Image = Timer.Properties.Resources.timer_23; }
+            if (k == 25) { pictureBox1.Image = Timer.Properties.Resources.timer_24; }
+            if (k == 26) { pictureBox1.Image = Timer.Properties.Resources.timer_25; }
+            if (k == 27) { pictureBox1.Image = Timer.Properties.Resources.timer_26; }
+            if (k == 28) { pictureBox1.Image = Timer.Properties.Resources.timer_27; }
+            if (k == 29) { pictureBox1.Image = Timer.Properties.Resources.timer_28; }
+            if (k == 30) { pictureBox1.Image = Timer.Properties.Resources.timer_29; }
+            if (k == 31) { pictureBox1.Image = Timer.Properties.Resources.timer_30; }
+            if (k == 32) { pictureBox1.Image = Timer.Properties.Resources.timer_31; }
+            if (k == 33) { pictureBox1.Image = Timer.Properties.Resources.timer_32; }
+            if (k == 34) { pictureBox1.Image = Timer.Properties.Resources.timer_33; }
+            if (k == 35) { pictureBox1.Image = Timer.Properties.Resources.timer_34; }
+            if (k == 36) { pictureBox1.Image = Timer.Properties.Resources.timer_35; }
+            if (k == 37) { pictureBox1.Image = Timer.Properties.Resources.timer_36; }
+            if (k == 38) { pictureBox1.Image = Timer.Properties.Resources.timer_37; }
+            if (k == 39) { pictureBox1.Image = Timer.Properties.Resources.timer_38; }
+            if (k == 40) { pictureBox1.Image = Timer.Properties.Resources.timer_39; }
+            if (k == 41) { pictureBox1.Image = Timer.Properties.Resources.timer_40; }
+            if (k == 42) { pictureBox1.Image = Timer.Properties.Resources.timer_41; }
+            if (k == 43) { pictureBox1.Image = Timer.Properties.Resources.timer_42; }
+            if (k == 44) { pictureBox1.Image = Timer.Properties.Resources.timer_43; }
+            if (k == 45) { pictureBox1.Image = Timer.Properties.Resources.timer_44; }
+            if (k == 46) { pictureBox1.Image = Timer.Properties.Resources.timer_45; }
+            if (k == 47) { pictureBox1.Image = Timer.Properties.Resources.timer_46; }
+            if (k == 48) { pictureBox1.Image = Timer.Properties.Resources.timer_47; }
+            if (k == 49) { pictureBox1.Image = Timer.Properties.Resources.timer_48; }
+            if (k == 50) { pictureBox1.Image = Timer.Properties.Resources.timer_49; }
+            if (k == 51) { pictureBox1.Image = Timer.Properties.Resources.timer_50; }
+            if (k == 52) { pictureBox1.Image = Timer.Properties.Resources.timer_52; }
+            if (k == 53) { pictureBox1.Image = Timer.Properties.Resources.timer_53; }
+            if (k == 54) { pictureBox1.Image = Timer.Properties.Resources.timer_54; }
+            if (k == 55) { pictureBox1.Image = Timer.Properties.Resources.timer_55; }
+            if (k == 56) { pictureBox1.Image = Timer.Properties.Resources.timer_56; }
+            if (k == 57) { pictureBox1.Image = Timer.Properties.Resources.timer_57; }
+            if (k == 58) { pictureBox1.Image = Timer.Properties.Resources.timer_58; }
+            if (k == 59) { pictureBox1.Image = Timer.Properties.Resources.timer_59; }
+           // if (k == 60) { pictureBox1.Image = Timer.Properties.Resources.timer_59; }
+            if (k == 60) { k = 0; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            flag_pause = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (flag_pause == true)
+            {
+                timer1.Enabled = true;
+                if (flag_stop == true)
+                {
+                 
+                    timer1.Enabled = true;
+                    time = textBox1.Text;
+                    s = Convert.ToInt32(time);
+                    m = s / 60;
+                    s %= 60;
+                    flag_stop = false;
+                    flag_pause = false;
+                }
+            }
+            else
+            {
+              
+                timer1.Enabled = true;
+                time = textBox1.Text;
+                s = Convert.ToInt32(time);
+                m = s / 60;
+                s %= 60;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Пожалуйста, введите время в секундах");
+            }
+            else
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.ForeColor = Color.Black;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+      
+    }
+}
